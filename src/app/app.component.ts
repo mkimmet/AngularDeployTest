c53d2b4179fab8135701bb0ba3fe6bc41b2e88b9
Mark Kimmet <mkimmet@gmail.com>
2019-03-14 16:38:42 -0400

    trying to get token from localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularDeployTest';
+
+  test(): void {
+    console.log(localStorage.getItem("token"));
+  }
 }
